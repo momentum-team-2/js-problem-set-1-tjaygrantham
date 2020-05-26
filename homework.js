06a510eb81262fc848a498968eba4e7d660aab5d
Tyler Grantham <tjaygrantham@gmail.com>
2020-05-26 12:02:25 -0400

    problems solved

--- homework.js
+++ homework.js
@@ -1,26 +1,50 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = "Addison";
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+candies = 20;
+people = 6;
+leftover = candies % people;
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+function greeting(name){
+    return `Hello, ${name}!`;
+}
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+function isOdd(num){
+    const result = num % 2;
+    if(result == 1 || result == -1)
+        return true;
+    return false;
+}
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(num){
+    if(num % 2 == 0)
+        return true;
+    return false;
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius(temp){
+    return (temp-32)*5/9;
+}
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(temp){
+    return temp*9/5+32;
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -28,10 +52,16 @@
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin(temp){
+    return fahrenheitToCelsius(temp) + 273.15;
+}
 
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser(num1, num2){
+    return Math.min(num1, num2);
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -44,6 +74,26 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
+function multigreeting(name, lang){
+    let greet;
+    switch(lang){
+        case 'en':
+            greet = 'Hello';
+            break;
+        case 'es':
+            greet = '¡Hola';
+            break;
+        case 'fr':
+            greet = 'Bonjour';
+            break;
+        case 'eo':
+            greet = 'Saluton';
+            break;
+        default:
+            return undefined;
+    }
+    return `${greet}, ${name}!`;
+}
 
 // 10. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
 // is the largest integer that, given two other integers, can be divided into them. For
@@ -71,3 +121,12 @@
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+function gcd(a, b){
+    while(a != b){
+        if(a > b)
+            a = a-b;
+        else
+            b = b-a;
+    }
+    return a;
+}
